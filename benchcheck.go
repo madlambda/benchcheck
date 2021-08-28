fdee677db655c630026994d82aef7ae71fade3c2
Tiago Katcipis <tiagokatcipis@gmail.com>
2021-08-28 18:47:49 +0200

    feat: make first test pass
    Error handling missing

--- benchcheck.go
+++ benchcheck.go
@@ -1,5 +1,11 @@
 package benchcheck
 
+import (
+	"encoding/json"
+	"fmt"
+	"os/exec"
+)
+
 // GetModule will download a specific version of a module and
 // return a directory where you can find the module code.
 // It leverages "go get" to do the job, so the returned directory
@@ -7,5 +13,15 @@ package benchcheck
 //
 // A non-nil error is returned if it fails.
 func GetModule(name string, version string) (string, error) {
-	return "", nil
+	// Reference: https://golang.org/ref/mod#go-mod-download
+	cmd := exec.Command("go", "mod", "download", "-json", fmt.Sprintf("%s@%s", name, version))
+	output, _ := cmd.CombinedOutput()
+	// TODO: handle errors
+
+	parsedResult := struct {
+		Error string // error loading module
+		Dir   string // absolute path to cached source root directory
+	}{}
+	json.Unmarshal(output, &parsedResult)
+	return parsedResult.Dir, nil
 }
