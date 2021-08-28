dbc18eea915b5cb42a20d94fae26f9dc30f59b0d
Tiago Katcipis <tiagokatcipis@gmail.com>
2021-08-28 16:20:33 +0200

    feat: add first failing test

--- benchcheck.go
+++ benchcheck.go
@@ -1,4 +1,11 @@
 package benchcheck
 
-func GetModule() {
+// GetModule will download a specific version of a module and
+// return a directory where you can find the module code.
+// It leverages "go get" to do the job, so the returned directory
+// should be considered read only (it is part of the Go cache).
+//
+// A non-nil error is returned if it fails.
+func GetModule(name string, version string) (string, error) {
+	return "", nil
 }
---
+++ benchcheck_test.go
@@ -0,0 +1,55 @@
+package benchcheck_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/madlambda/benchcheck"
+)
+
+func TestGetModule(t *testing.T) {
+	tests := []struct {
+		desc          string
+		moduleName    string
+		moduleVersion string
+		wantErr       bool
+	}{
+		{
+			desc:          "ValidVersion",
+			moduleName:    "github.com/madlambda/jtoh",
+			moduleVersion: "v0.1.0",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			modDir, err := benchcheck.GetModule(test.moduleName, test.moduleVersion)
+
+			if test.wantErr {
+				if err == nil {
+					t.Fatalf("want error, got nil")
+				}
+				return
+			}
+
+			if err != nil {
+				t.Fatalf(
+					"benchcheck.GetModule(%q, %q): unexpected error : %v",
+					test.moduleName,
+					test.moduleVersion,
+					err,
+				)
+			}
+
+			fileinfo, err := os.Stat(modDir)
+
+			if err != nil {
+				t.Fatalf("os.Stat(%q): unexpected error : %v", modDir, err)
+			}
+
+			if !fileinfo.IsDir() {
+				t.Fatalf("want %q to be a dir, details : %v", modDir, fileinfo)
+			}
+		})
+	}
+}
