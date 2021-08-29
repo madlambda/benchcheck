04aff61b5a21bd031fc231fa1fd1d25e613ea91f
Tiago Katcipis <tiagokatcipis@gmail.com>
2021-08-29 17:53:21 +0200

    feat: improve checking of module version

--- benchcheck_test.go
+++ benchcheck_test.go
@@ -2,17 +2,23 @@ package benchcheck_test
 
 import (
 	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/madlambda/benchcheck"
 )
 
 func TestGetModule(t *testing.T) {
+	// Not extremely happy with the way the tests are coupled to changes
+	// on jtoh, like new releases would break latest logic here.
+	// But not testing this integrated with actual repos would be almost useless.
 	tests := []struct {
 		desc          string
 		moduleName    string
 		moduleVersion string
 		wantErr       bool
+		wantVersion   string
 	}{
 		{
 			desc:          "ValidVersion",
@@ -23,11 +29,13 @@ func TestGetModule(t *testing.T) {
 			desc:          "UsingLatest",
 			moduleName:    "github.com/madlambda/jtoh",
 			moduleVersion: "latest",
+			wantVersion:   "v0.1.0",
 		},
 		{
 			desc:          "UsingCommitSha",
 			moduleName:    "github.com/madlambda/jtoh",
 			moduleVersion: "5cd825858d7dcc41c3b453ed10ecf0983b139243",
+			wantVersion:   "v0.1.1-0.20210731193031-5cd825858d7d",
 		},
 		{
 			desc:          "InvalidVersion",
@@ -72,6 +80,33 @@ func TestGetModule(t *testing.T) {
 			if !fileinfo.IsDir() {
 				t.Fatalf("want %q to be a dir, details : %v", modDir, fileinfo)
 			}
+
+			if test.wantVersion == "" {
+				test.wantVersion = test.moduleVersion
+			}
+
+			gotModuleVersion := getModuleVersion(t, modDir)
+			if gotModuleVersion != test.wantVersion {
+				t.Fatalf(
+					"got version %q from mod dir %q, wanted %q",
+					gotModuleVersion,
+					modDir,
+					test.wantVersion,
+				)
+			}
 		})
 	}
+}
+
+func getModuleVersion(t *testing.T, modDir string) string {
+	// This function abuses private details abou the modDir,
+	// more specifically, the fact it is a Go cache dir.
+	// But it is at least decoupled from specific version systems.
+	// We couple on whatever interfaces Go provides.
+	modNameVersion := filepath.Base(modDir)
+	parsed := strings.Split(modNameVersion, "@")
+	if len(parsed) <= 1 {
+		t.Fatalf("module cache dir supposed to be on the form 'module@version' got: %q", modDir)
+	}
+	return strings.Join(parsed[1:], "")
 }
