095236c40a2ac9eb056eab98de17ef7d1cf342fc
Tiago Katcipis <tiagokatcipis@gmail.com>
2022-04-18 18:49:13 +0200

    chore: add some fake coverage to fake functions

--- internal/fake/fake_test.go
+++ internal/fake/fake_test.go
@@ -6,6 +6,11 @@ import (
 	"github.com/madlambda/benchcheck/internal/fake"
 )
 
+func TestFake(t *testing.T) {
+	// playing around with coverage
+	fake.Do()
+}
+
 func BenchmarkFake(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		fake.Do()
